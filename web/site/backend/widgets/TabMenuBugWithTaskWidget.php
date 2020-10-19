d298905bb059905dd9a2efbb06ca576f632ea85b
peichong <fupeichong@gmail.com>
2020-10-19 22:44:20 +0800

    enable My Submissions tab for staff users to filter their submitted bugs

--- web/site/backend/widgets/TabMenuBugWithTaskWidget.php
+++ web/site/backend/widgets/TabMenuBugWithTaskWidget.php
@@ -16,24 +16,27 @@ class TabMenuBugWithTaskWidget extends Widget
 
     public function init()
     {
-        parent::init();        
+        parent::init();
     }
 
     public function run()
     {
-        $active1 = $active2 = $active3 = "";
+        $active1 = $active2 = $active3 = $active4 = "";
         if ($this->page == "index") {
             $active1 = "active";
         } else if ($this->page == "tasks") {
             $active2 = "active";
         } else if ($this->page == "closed") {
             $active3 = "active";
+        } else if ($this->page == 'user-submissions') {
+            $active4 = "active";
         }
-        
+
         $link1 =  Url::to(["bug/index"]);
         $link2 =  Url::to(["bug/tasks"]);
         $link3 =  Url::to(["bug/closed"]);
-        
+        $link4 =  Url::to(["bug/user-submissions"]);
+
         $content = <<<HEREDOC
 <ul class="nav nav-tabs">
     <li class="nav-item">
@@ -44,6 +47,9 @@ class TabMenuBugWithTaskWidget extends Widget
     </li>
     <li class="nav-item">
         <a class="nav-link $active3" href="$link3">Closed</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link $active4" href="$link4">My Submissions</a>
     </li>
 </ul>
 <br>
