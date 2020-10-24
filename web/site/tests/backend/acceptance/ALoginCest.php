8fbf3acbcffe99a4bbb04a6c4e539cb2dc0fa793
Eddie Lim <eddielinoofficial@gmail.com>
2020-10-24 19:24:59 +0800

    unit test configs ok!!!

--- web/site/tests/backend/acceptance/ALoginCest.php
+++ web/site/tests/backend/acceptance/ALoginCest.php
@@ -1,19 +1,16 @@
 <?php
 
 use tests\backend\_pages\LoginPage;
-use tests\backend\_pages\OtpPage;
 use tests\backend\AcceptanceTester as AT;
 
 /* @var $scenario Codeception\Scenario */
 class ALoginCest {
 
 	public $loginPage;
-	public $otpPage;
 
 	public function _before(AT $I) {
 		sleep(5); 
 		$this->loginPage = new LoginPage();
-		$this->otpPage = new OtpPage();
     }
 
     public function _after(AT $I) {
@@ -21,110 +18,23 @@ class ALoginCest {
     }
 
  	public function login(AT $I) {	
-        $I->amOnPage('/login');
-        $I->wantTo('try to login with invalid mobile number');
-		$this->loginPage->login($I, '65', 'webmaster1234');
+        $I->amOnPage('/sign-in/login');
+        $I->wantTo('try to login with invalid account');
+		$this->loginPage->login($I, 'wronguser', 'wrongpassword');
 		if (method_exists($I, 'wait')) {
 		    $I->wait(3); // only for selenium
 		}
-		$I->see('Mobile Number is invalid.');
+		$I->see('Incorrect username or password.');
     }
 
     public function login2(AT $I) {	
-        $I->amOnPage('/login');
-        $I->wantTo('try to login with empty mobile number');
-		$this->loginPage->login($I, '65', '');
+        $I->amOnPage('/sign-in/login');
+        $I->wantTo('login');
+		$this->loginPage->login($I, 'webmaster', 'webmaster');
 		if (method_exists($I, 'wait')) {
 		    $I->wait(3); // only for selenium
 		}
-		$I->see('Mobile Number cannot be blank.');
+        $I->see('Bug Tracking Sys');
     }
 
- 	public function login3(AT $I) {	
-        $I->amOnPage('/login');
-        $I->wantTo('try to login with not existing user');
-		$this->loginPage->login($I, '65', '93732062');
-		if (method_exists($I, 'wait')) {
-		    $I->wait(3); // only for selenium
-		}
-		$I->see('No such user.');
-
-        // $I->waitForElement(Product::$pageLogo);
-        // $I->seeElement(Product::$pageLogo.' img');
-    }
-
- 	public function login4(AT $I) {	
-        $I->wantTo('Login');
-        $I->amOnPage('/login');
-        $this->loginPage->login($I, '65', '93732061');
-
-        if (method_exists($I, 'wait')) {
-		    $I->wait(3); // only for selenium
-		}
-		$I->see('One-Time Pin');
-        // $I->waitForElement(Product::$pageLogo);
-        // $I->seeElement(Product::$pageLogo.' img');
-    }
-
-    public function otp(AT $I) {	
-        $I->wantTo('try to login with empty otp');
-        $I->amOnPage('/otp');
-        $this->otpPage->login($I, '');
-
-        if (method_exists($I, 'wait')) {
-		    $I->wait(3); // only for selenium
-		}
-		$I->see('token cannot be blank.');
-        // $I->seeElement(Product::$pageLogo.' img');
-    }
-
-    public function otp2(AT $I) {	
-        $I->wantTo('try to login with wrong otp');
-        $I->amOnPage('/otp');
-        $this->otpPage->login($I, '111112');
-
-        if (method_exists($I, 'wait')) {
-		    $I->wait(3); // only for selenium
-		}
-		$I->see('invalid token.');
-		// $I->see('DASHBOARD.');
-        // $I->seeElement(Product::$pageLogo.' img');
-    }
-
-    public function otp3(AT $I) {	
-        $I->wantTo('enter valid otp');
-        $I->amOnPage('/otp');
-        $this->otpPage->login($I, '111111');
-
-        if (method_exists($I, 'wait')) {
-		    $I->wait(3); // only for selenium
-		}
-		// $I->see('DASHBOARD.');
-        // $I->seeElement(Product::$pageLogo.' img');
-    }
 }
-
-
-
-// $I->amGoingTo('try to login with empty otp');
-// $otpPage->login('');
-// if (method_exists($I, 'wait')) {
-//     $I->wait(3); // only for selenium
-// }
-// $I->see('token cannot be blank.');
-
-
-// $I->amGoingTo('try to login with invalid otp');
-// $otpPage->login('111112');
-// if (method_exists($I, 'wait')) {
-//     $I->wait(3); // only for selenium
-// }
-// $I->see('invalid token.');
-
-
-// $I->amGoingTo('try to login with string value');
-// $otpPage->login('aaaaaa');
-// if (method_exists($I, 'wait')) {
-//     $I->wait(3); // only for selenium
-// }
-// $I->see('invalid token.');
